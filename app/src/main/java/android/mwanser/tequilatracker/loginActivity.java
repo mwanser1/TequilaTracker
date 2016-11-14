4bdf1423f43154cbc72be0fc2f39d1a1e0315e3e
Michael Wanser <wanserm@gmail.com>
2016-11-14 11:38:48 -0500

    tabview update

--- app/src/main/java/android/mwanser/tequilatracker/loginActivity.java
+++ app/src/main/java/android/mwanser/tequilatracker/loginActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
 
+    //fun
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
